2cb06076d1bc52bff3d949b37794c0076ec6510e
Miguel Pombo <mp96dev@gmail.com>
2023-01-11 12:21:25 -0300

    feat: saving file inside our project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿namespace TexEditor {
+﻿using System.IO ;
+namespace TexEditor {
     class Program {
         static void Main(string[] args) {
             MenuApp();
@@ -37,8 +38,28 @@
 
             } while (Console.ReadKey().Key != ConsoleKey.Escape);
 
-            Console.Write(text);
+            Save_File(text);
             
+        }
+
+        static void Save_File(string text) {
+            string path = "./files";
+            string filename;
+            bool check_directory = Directory.Exists(path);
+
+            Console.WriteLine("How will be named your file?");
+            filename = Console.ReadLine();
+
+            if (check_directory == false) {
+               Directory.CreateDirectory(path);
+            }
+
+            using (var file = new StreamWriter($"{path}/{filename}.txt")) {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"File saved in {path} successfully");
+            MenuApp();
         }
     }
 }
